0403fe6c78aa9d1f1b34c91fbe4e529ccc71ed15
BJU <joonguk90@gmail.com>
2018-01-03 17:30:26 -0800

    P2029 commit

--- SWExpertAcademy/P2029/P2029.cpp
+++ SWExpertAcademy/P2029/P2029.cpp
@@ -3,12 +3,15 @@
 int main()
 {
   int T;
-
+  int num1, num2;
   scanf("%d", &T);
 
   for(int test_case = 1; test_case <= T; test_case++)
   {
-    
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    printf("#%d %d %d\n", test_case, num1 / num2, num1 % num2);
   }
 
   return 0;
