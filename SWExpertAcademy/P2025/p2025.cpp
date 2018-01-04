b7cc3b2f55eb0ef3f37898adbecd76e86c64296f
BJU <joonguk90@gmail.com>
2018-01-03 17:55:21 -0800

    P2025 commit

---
+++ SWExpertAcademy/P2025/p2025.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int main()
+{
+  int num = 0;
+  int sum = 0;
+  int i;
+
+  scanf("%d", &num);
+
+  for (i = 1; i <= num; i++)
+  {
+    sum = i;
+  }
+
+  printf("%d", sum);
+
+  return 0;
+}
