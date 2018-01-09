c02a6a51a2a9c93c3987db442a1cf57df0229a4d
BJU <joonguk90@gmail.com>
2018-01-09 15:53:33 -0800

    P1983 진행중 commit

---
+++ SWExpertAcademy/P1983/P1983.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main()
+{
+  int T = 0;
+
+  scanf("%d", &T);
+
+  for (test_case = 1; test_case <= T; test_case++)
+  {
+
+    printf("#%d\n", test_case);
+  }
+
+
+  return 0;
+}
